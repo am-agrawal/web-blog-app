f89de04c87d70eebd76a621cdc106dc7ce758b62
Aman Kumar Agrawal <582002aman@gmail.com>
2022-09-11 15:30:32 +0530

    complete project

---
+++ components/Header.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+import Link from 'next/link';
+import { getCategories } from '../services';
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+
+  return (
+		<div className="from-primary to-secondary bg-gradient-to-tr container mx-auto px-10 pb-8">
+			<div className="w-full inline-block py-8">
+				<div className="md:float-left block">
+					<Link href="/">
+						<span className="cursor-pointer font-bold text-4xl text-white">
+							Blog App
+						</span>
+					</Link>
+				</div>
+				<div className="hidden md:float-left md:contents">
+					{categories.map((category, index) => (
+						<Link key={index} href={`/category/${category.slug}`}>
+							<span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+								{category.name}
+							</span>
+						</Link>
+					))}
+				</div>
+			</div>
+		</div>
+	)
+};
+
+export default Header;
